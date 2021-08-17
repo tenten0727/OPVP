721bd079da0684a80bace68a18f5a9d317f160c2
TakatoYoshikawa <tenten0727@icloud.com>
2021-08-17 23:30:52 +0900

    [ADD] utils.py

---
+++ utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 加重平均価格
+def calc_wap(df):
+    wap = (df['bid_price1'] * df['ask_size1'] + df['ask_price1'] * df['bid_size1']) / (df['bid_size1'] + df['ask_size1'])
+    return wap
+
+def calc_wap2(df):
+    wap = (df['bid_price2'] * df['ask_size2'] + df['ask_price2'] * df['bid_size2']) / (df['bid_size2'] + df['ask_size2'])
+    return wap
+
+def log_return(list_stock_prices):
+    return np.log(list_stock_prices).diff() 
+
+def realized_volatility(series):
+    return np.sqrt(np.sum(series**2))
+
+def count_unique(series):
+    return len(np.unique(series))
+
+# ref https://www.kaggle.com/corochann/permutation-importance-for-feature-selection-part1
+def calc_model_importance(model, feature_names=None, importance_type='gain'):
+    importance_df = pd.DataFrame(
+        model.feature_importance(importance_type=importance_type),
+        index=feature_names,
+        columns=['importance']
+    ).sort_values('importance')
+    
+    return importance_df
+
+def plot_importance(importance_df, title='', save_filepath=None, figsize=(8, 12)):
+    fig, ax = plt.subplots(figsize=figsize)
+    importance_df.plot.barh(ax=ax)
+    if title:
+        plt.title(title)
+    plt.tight_layout()
+    if save_filepath is None:
+        plt.show()
+    else:
+        plt.savefig(save_filepath)
+    plt.close()
+    
+def calc_mean_importance(importance_df_list):
+    mean_importance = np.mean(np.array([df['importance'].values for df in importance_df_list]), axis=0)
+    mean_df = importance_df_list[0].copy()
+    mean_df['importance'] = mean_importance
+    return mean_df
+
+def rmspe(y_true, y_pred):
+    return  (np.sqrt(np.mean(np.square((y_true - y_pred) / y_true))))
+
+def feval_RMSPE(preds, lgbm_train):
+    labels = lgbm_train.get_label()
+    return 'RMSPE', round(rmspe(y_true = labels, y_pred = preds), 5), False
